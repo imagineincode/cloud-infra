d0edd37635cdad88d12eb94b83e4ba749dfb9fdf
Stephen <stephen@imagineincode.com>
2025-01-03 12:59:18 -0600

    adding aws baseline config

---
+++ aws/modules/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of availability zones for subnets"
+  type        = list(string)
+}
